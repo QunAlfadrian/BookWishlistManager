18f0442aefc1f9dcabd72cf0c319476e72a27bc0
PAVILION GAMING <PAVILION GAMING@QUNALFADRIAN>
2023-04-27 16:23:58 +0700

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWishlistManager {
+    public class Book {
+        public string title { get; set; }
+        public int price { get; set; }
+        public bool owned { get; set; }
+
+        public void PrintComponents() {
+            if (title.Length <= 7) {
+                Console.WriteLine("{0}\t\t\t\t{1}\t\t{2}", title, price, owned);
+            } else if (title.Length > 7 && title.Length <= 14) {
+                Console.WriteLine("{0}\t\t\t{1}\t\t{2}", title, price, owned);
+            } else {
+                Console.WriteLine("{0}\t\t{1}\t\t{2}", title, price, owned);
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+
+namespace BookWishlistManager {
+    internal class Program {
+        // path variable
+        public static string filePath = "bookList.csv";
+
+        static void Main(string[] args) {
+            // loop variable
+            bool pilihLagi = true;
+
+            // input variable
+            int menuInput;
+
+            Console.WriteLine("Selamat Datang");
+            ReadFile(ConvertToArr(filePath));
+
+            while (pilihLagi) {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Pilih Menu: ");
+                Console.WriteLine(" <1> Read");
+                Console.WriteLine(" <2> Update");
+                Console.WriteLine(" <3> Write");
+                Console.WriteLine(" <4> Keluar");
+
+                menuInput = Convert.ToInt32(Console.ReadLine());
+
+                switch (menuInput) {
+                    case 1:
+                        ReadFile(ConvertToArr(filePath));
+                        break;
+                    case 2:
+                        Console.WriteLine("Update");
+                        break;
+                    case 3:
+                        AddEntry();
+                        break;
+                    case 4:
+                        pilihLagi = false;
+                        break;
+                    default:
+                        Console.WriteLine("Pilihan tidak valid");
+                        break;
+                }
+            }
+
+        }
+
+        // read methods
+        static List<Book> ConvertToArr(string path) {
+            var lines = File.ReadAllLines(path);
+            var books = new List<Book>();
+
+            foreach (var line in lines) {
+                var components = line.Split(',');
+                var book = new Book() {
+                    title = components[0],
+                    price = Convert.ToInt32(components[1]),
+                    owned = Convert.ToBoolean(components[2]),
+                };
+                books.Add(book);
+            }
+            return books;
+        }
+
+        static void ReadFile(List<Book> books) {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Judul\t\t\t\tHarga\t\tSudah Beli?\n");
+            foreach (var book in books) {
+                book.PrintComponents();
+            }
+            Console.WriteLine();
+        }
+
+        // write methods
+        static Book CreateBook() {
+            char isOwned;
+            Book book = new Book();
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Masukkan judul Buku");
+            book.title = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\nMasukkan harga Buku");
+            book.price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nApakah sudah punya? [y|n]");
+            isOwned = Convert.ToChar(Console.ReadLine());
+
+            if (isOwned == 'y') {
+                book.owned = true;
+            } else {
+                book.owned = false;
+            }
+
+            return book;
+        }
+
+        static void WriteFile(Book book, string path) {
+            string bookTitle = book.title;
+            string bookPrice = Convert.ToString(book.price);
+            string bookOwned = Convert.ToString(book.owned);
+
+            try {
+                using (StreamWriter file = new StreamWriter(@path, true)) {
+                    file.WriteLine(bookTitle + "," + bookPrice + "," + bookOwned);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void AddEntry() {
+            Book book = CreateBook();
+
+            WriteFile(book, filePath);
+        }
+    }
+}
