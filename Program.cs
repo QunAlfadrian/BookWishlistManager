3dbba1b52410c57f461a5b499fd495b48127f179
Qun Alfadrian <qunadeian@gmail.com>
2023-05-03 06:55:50 +0700

    Add FileReader class Add ReadToArray method

--- Program.cs
+++ Program.cs
@@ -6,19 +6,23 @@ namespace BookWishlistManager {
         // path variable
         public static string FilePath = @"C:\Users\PAVILION GAMING\Documents\Book Saves\books.csv";
         public static FileWriter writer = new FileWriter();
+        public static FileReader reader = new FileReader();
         public static Book book = new Book();
 
         static void Main(string[] args) {
             CreateFile(FilePath);
 
-            var lineList = writer.LineList(FilePath);
-            writer.AppendLines(FilePath, lineList);
+            //var lineList = writer.LineList(FilePath);
+            //writer.AppendLines(FilePath, lineList);
 
-            var lines = File.ReadAllLines(FilePath);
+            string[] lines;
+            lines = reader.ReadToArray(FilePath);
             
             foreach (var line in lines) {
                 Console.WriteLine(line);
             }
+
+            Console.WriteLine("\n"+lines);
         }
 
         static void CreateFile(string filePath) {
